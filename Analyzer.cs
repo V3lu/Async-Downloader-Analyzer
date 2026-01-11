d92fc24bd694739009fc46320a0ffbc0729de4ac
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2026-01-11 16:59:48 +0100

    Initialized structure

---
+++ Analyzer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Async_Downloader
+{
+    internal class Analyzer
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Async_Downloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
